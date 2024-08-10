0f4c8058730e27a37f06a9fbeb83bf86061f616b
DoctorDoofen <and.bennett0930@gmail.com>
2024-08-10 18:36:35 -0500

    bubble sort done

--- problems/bubble-sorting.js
+++ problems/bubble-sorting.js
@@ -2,12 +2,16 @@ const bubbleSort = (arr) => {
 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > arr[i + 1]) {
-            arr[i] = arr[i + 1]
-            arr[i + 1] = arr[i]
-        } else
+            [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+            bubbleSort(arr)
+        }
     }
     return arr
 }
+
+let arr1 = [5, 7, 6, 1, 9, 4, 8, 2, 3, 10]
+console.log(bubbleSort(arr1))
+
 // 1. If the current value is greater than its neighbor to the right
 // 2. Swap those values
 // 3. If you get to the end of the array and no swaps have occurred, return
--- problems/merge-sorting.js
+++ problems/merge-sorting.js
@@ -1,3 +1,46 @@
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+
+    left = mergeSort(left)
+    right = mergeSort(right)
+
+    return merge(left, right)
+
+}
+
+
+function merge(arr1, arr2) {
+    let res = [];
+
+    let arr1Index = 0
+    let arr2Index = 0
+
+
+    while (arr1Index < arr1.length && arr2Index < arr2.length) {
+        if (arr1[arr1Index] < arr2[arr2Index]) {
+            res.push(arr1[arr1Index])
+            arr1Index++
+        } else {
+            res.push(arr2[arr2Index])
+            arr2Index++
+        }
+    }
+    if (arr1Index < arr1.length) {
+        res = res.concat(arr1.slice(arr1Index))
+    } else {
+        res = res.concat(arr2.slice(arr2Index))
+    }
+    return res
+}
+
+
+let test = [1, 5, 3, 2, 4, 6, 8, 7, 9, 10]
+console.log(mergeSort(test))
+
 // Sorting Function
 // 1. Check if the input is length 1 or less.
 // 2. If so, it's already sorted: return.
