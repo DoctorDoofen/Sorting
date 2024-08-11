378a14307a9cc5c5c3fd0b30d1674025d07337a0
DoctorDoofen <and.bennett0930@gmail.com>
2024-08-10 20:59:51 -0500

    selecton sort done

--- problems/bubble-sorting.js
+++ problems/bubble-sorting.js
@@ -1,6 +1,6 @@
 const bubbleSort = (arr) => {
 
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
         if (arr[i] > arr[i + 1]) {
             [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
             bubbleSort(arr)
--- problems/selection-sorting.js
+++ problems/selection-sorting.js
@@ -1,3 +1,21 @@
+function selectionSort(arr) {
+
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        let min = i
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        [arr[i], arr[min]] = [arr[min], arr[i]]
+    }
+    return arr
+}
+
+debugger
+let arr1 = [5, 7, 6, 1, 9, 4, 8, 2, 3, 10]
+console.log(selectionSort(arr1))
 // 1. Set MIN to location 0.
 // 2. Search the minimum element in the list.
 // 3. Swap with value at location MIN.
